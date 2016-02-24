317fbe0c543236727993eeb6d9e9394d378c401f
ww5365 <ww5365@163.com>
2016-02-24 14:32:05 +0800

    first create by ww

---
+++ inc/test.h
@@ -0,0 +1,15 @@
+/*
+ * test.h
+ *
+ *  Created on: 2016Äê2ÔÂ24ÈÕ
+ *      Author: wangwei69
+ */
+
+#ifndef TEST_H_
+#define TEST_H_
+
+
+
+
+
+#endif /* TEST_H_ */
---
+++ src/1.cpp
@@ -0,0 +1,15 @@
+/*
+ * 1.cpp
+ *
+ *  Created on: 2016Äê2ÔÂ24ÈÕ
+ *      Author: wangwei69
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "hello word!" <<endl;
+}
+
